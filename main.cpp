43ad57343f58c08d31a0ebb5a02141ae6082847f
Bogdanbas16 <113505138+Bogdanbas16@users.noreply.github.com>
2023-01-21 04:16:02 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  calc
+//
+//  Created by Bogdan Basarab on 19.01.2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
